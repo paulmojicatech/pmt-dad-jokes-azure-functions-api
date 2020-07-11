03f1edd76214649ba32e3195243b2338e3c9a406
Paul Mojica <paulmojicatech@gmail.com>
2020-07-10 21:25:33 -0400

    #1 add interface for adapter

---
+++ PmtDadJokesDAO/Interfaces/IDBAdapter.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using PmtDadJokesDAO.Models;
+
+namespace PmtDadJokesDAO.Interfaces
+{
+    public interface IDBAdapter
+    {
+        IEnumerable<object>[] Get(QueryParameter[] parameters);
+        ResultStatus Update(QueryParameter[] parameters);
+        ResultStatus Add(QueryParameter[] parameters);
+        ResultStatus Delete(QueryParameter[] paramters);
+    }
+}
---
+++ PmtDadJokesDAO/Mongo/MongoDBAdapter.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace PmtDadJokesDAO
+{
+    public class MongoDBAdapter
+    {
+
+    }
+}
